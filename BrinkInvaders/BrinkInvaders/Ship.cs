1f7a265e8cd95b6f66ed4b385318151b454abec9
redouane59 <bali.redouane@gmail.com>
2013-05-23 07:37:20 -0700

    moine

--- BrinkInvaders/BrinkInvaders/Ship.cs
+++ BrinkInvaders/BrinkInvaders/Ship.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace BrinkInvaders
 {
-    class Ship
+    class Ship:DynamicElement
     {
         private string label;
         private int height;
         private int id;
         private ShipView view;
+
+        public Ship(string label, int height, int id, ShipView view, int speed,int width, Color color, Point position)
+            : base(speed, width, color, position)
+        {
+            this.label = label;
+            this.height = height;
+            this.id = id;
+            this.view = view;
+        }
 
         public ShipView View
         {
@@ -18,26 +28,21 @@ namespace BrinkInvaders
             set { view = value; }
         }
 
-
         public int Id
         {
             get { return id; }
-        }
-        
+        }        
 
         public int Height
         {
             get { return height; }
             set { height = value; }
-        }
-        
+        }        
 
         public string Label
         {
             get { return label; }
             set { label = value; }
         }
-        
-
     }
 }
