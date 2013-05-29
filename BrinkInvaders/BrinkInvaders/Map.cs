9b4a7273dd876d7db6f7b599a9b7c0b132ae57f5
Antoine Guillon <antoine.guillon59@gmail.com>
2013-05-29 05:03:08 -0700

    Added ships and balls to map

--- BrinkInvaders/BrinkInvaders/Map.cs
+++ BrinkInvaders/BrinkInvaders/Map.cs
@@ -10,15 +10,19 @@ namespace BrinkInvaders
         public List<BasicBrick> Bricks { get; set; }
         public int Height { get; set; }
         public int Width { get; set; }
+        public List<Ship> Ships { get; set; }
+        public List<Ball> Balls { get; set; }
 
-        public Map(int height,int width, List<BasicBrick> bricks)
+
+        public Map(int height, int width, List<BasicBrick> bricks, List<Ship> ships, List<Ball> balls)
         {
             this.Height = height;
             this.Width = width;
             this.Bricks = bricks;
+            this.Ships = ships;
+            this.Balls = balls;
         }
-
-        public Map(int height, int width):this(height,width,new List<BasicBrick>()){}
+        public Map(int height, int width) : this(height, width, new List<BasicBrick>(), new List<Ship>(), new List<Ball>()) { }
         public Map():this(0,0){}
     }
 }
