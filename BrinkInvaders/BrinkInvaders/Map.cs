605782b7ef379c1ec106f988ff7ba1add498336a
Antoine Guillon <antoine.guillon59@gmail.com>
2013-05-29 05:49:15 -0700

    Map's namespace

--- BrinkInvaders/BrinkInvaders/Map.cs
+++ BrinkInvaders/BrinkInvaders/Map.cs
@@ -1,28 +1,32 @@
-﻿using System;
+﻿using BrickInvaders.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace BrinkInvaders
 {
-    class Map
+    namespace Model
     {
-        public List<BasicBrick> Bricks { get; set; }
-        public int Height { get; set; }
-        public int Width { get; set; }
-        public List<Ship> Ships { get; set; }
-        public List<Ball> Balls { get; set; }
-
-
-        public Map(int height, int width, List<BasicBrick> bricks, List<Ship> ships, List<Ball> balls)
+        class Map
         {
-            this.Height = height;
-            this.Width = width;
-            this.Bricks = bricks;
-            this.Ships = ships;
-            this.Balls = balls;
+            public List<BasicBrick> Bricks { get; set; }
+            public int Height { get; set; }
+            public int Width { get; set; }
+            public List<Ship> Ships { get; set; }
+            public List<Ball> Balls { get; set; }
+
+
+            public Map(int height, int width, List<BasicBrick> bricks, List<Ship> ships, List<Ball> balls)
+            {
+                this.Height = height;
+                this.Width = width;
+                this.Bricks = bricks;
+                this.Ships = ships;
+                this.Balls = balls;
+            }
+            public Map(int height, int width) : this(height, width, new List<BasicBrick>(), new List<Ship>(), new List<Ball>()) { }
+            public Map() : this(0, 0) { }
         }
-        public Map(int height, int width) : this(height, width, new List<BasicBrick>(), new List<Ship>(), new List<Ball>()) { }
-        public Map():this(0,0){}
     }
 }
