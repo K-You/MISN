9cf009798f6666c431e85293b3c70c5e88f61783
Antoine Guillon <antoine.guillon59@gmail.com>
2013-05-31 05:26:53 -0700

    AJout de prise en compte du niveau pour la génération

--- BrinkInvaders/BrinkInvaders/BasicMapGenerator.cs
+++ BrinkInvaders/BrinkInvaders/BasicMapGenerator.cs
@@ -14,37 +14,45 @@ namespace BrickInvaders
 
         public class BasicMapGenerator : MapGenerator
         {
-
+            public const int MINWIDTH = 6;
+            public const int MINHEIGHT = 6;
+            public BasicMapGenerator()
+            {
+                this.rand = new Random();
+            }
+            private Random rand;
             public List<BasicBrick> generateMap(int level, int width, int height)
             {
-                if (width <= 6 && height <= 10)
+                if (MINWIDTH <= 6 && MINHEIGHT <= 10)
                 {
                     throw new ArgumentOutOfRangeException();
                 }
                 List<BasicBrick> tempList = new List<BasicBrick>();
-                Random rand = new Random();
+                
+                double levelOffset = Math.Sqrt(2 * Math.Sqrt(level));//Calcule l'offset de probabilité en fonction du niveau, selon une évolution type racine un peu ralentie
+
                 bool previous = true;
                 int since = 0;
                 for (int i = height - 1; i > 0; i--)
                 {
-                    if (previous && since < 3)
+                    if (previous && since < 3)//Il y a eu des briques avant donc j'attends 3 lignes minimum
                     {
                         since++;
                     }
                     else
                     {
-                        previous = rand.Next(0, 10) <= 4;
+                        previous = rand.Next(0, 10) <= (4 + (levelOffset-2));//=Est-ce que je mets des briques sur cette ligne ou j'attends la prochaine
                         if (previous)
                         {
                             since = 0;
                             int x = rand.Next(0, width);
-                            tempList.Add(new BasicBrick(new Vector2D(x, i)));
-                            if (x <= (width / 2) - 3 && rand.Next(0, 10) <= 3)
+                            tempList.Add(new BasicBrick(new Vector2D(x, i)));//J'ajoute une brique
+                            if (x <= (width / 2) - 3 && rand.Next(0, 10) <= (3 + levelOffset))//Si elle est pas centrée et si le rand dit oui, je mets une brique de l'autre côté, horizontalement
                             {
                                 x = rand.Next(0, (width / 2) - 2);
                                 tempList.Add(new BasicBrick(new Vector2D(width - 1 - x, i)));
                             }
-                            else if (x >= (width / 2) + 3 && rand.Next(0, 10) <= 3)
+                            else if (x >= (width / 2) + 3 && rand.Next(0, 10) <= (3 + levelOffset))//Ou de l'autre côté
                             {
                                 x = rand.Next((width / 2) + 2, width);
                                 tempList.Add(new BasicBrick(new Vector2D(width - 1 - x, i)));
@@ -63,19 +71,19 @@ namespace BrickInvaders
                     {
                         for (nY = y - 1; nY <= y + 1; nY++)
                         {
-                            if (nX >= 0 && nX <= width - 1 && nY >= 0 && nY <= height - 1 && rand.Next(0, 10) <= 7)
+                            if (nX >= 0 && nX <= width - 1 && nY >= 0 && nY <= height - 1 && rand.Next(0, 10) <= (6+levelOffset))//Si rand veut bien je rajoute les briques autour des briques du dessus pour dessiner des "vaisseaux"
                             {
                                 list.Add(new BasicBrick(new Vector2D(nX, nY)));
                             }
                         }
                     }
                     nX = x - 2; nY = y;
-                    if (nX >= 0 && nX <= width - 1 && nY >= 0 && nY <= height - 1 && rand.Next(0, 10) <= 7)
+                    if (nX >= 0 && nX <= width - 1 && nY >= 0 && nY <= height - 1 && rand.Next(0, 10) <= (6 + levelOffset))//idem
                     {
                         list.Add(new BasicBrick(new Vector2D(nX, nY)));
                     }
                     nX = x + 2;
-                    if (nX >= 0 && nX <= width - 1 && nY >= 0 && nY <= height - 1 && rand.Next(0, 10) <= 7)
+                    if (nX >= 0 && nX <= width - 1 && nY >= 0 && nY <= height - 1 && rand.Next(0, 10) <= (6 + levelOffset))//idem
                     {
                         list.Add(new BasicBrick(new Vector2D(nX, nY)));
                     }
@@ -86,13 +94,13 @@ namespace BrickInvaders
 
             public Ship generateShip()
             {
-                return new Ship("Basic ship!");
+                return new Ship("Basic ship!");//A compléter je pense
             }
 
-            public void test()
+            public void test(int p)//A supprimer quand ça sera OK
             {
                 int width = 40, height = 60;
-                List<BasicBrick> list = this.generateMap(1, width, height);
+                List<BasicBrick> list = this.generateMap(p, width, height);
                 char[,] lines = new char[height, width];
                 foreach (BasicBrick item in list)
                 {
@@ -114,6 +122,11 @@ namespace BrickInvaders
                     Console.WriteLine(s + '|');
                 }
                 Console.WriteLine(delimiter);
+            }
+
+            public void test()
+            {
+                this.test(1);
             }
         }
     }
